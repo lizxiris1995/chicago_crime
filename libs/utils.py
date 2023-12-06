5d9d04097f47ba74d61bc80082e0d026570e3b1f
Zexi Li <zli735@gatech.edu>
2023-12-06 18:49:32 -0500

    add util function to convert pandas to tensor

--- libs/utils.py
+++ libs/utils.py
@@ -1,6 +1,16 @@
 import os
 import os.path
 import pandas as pd
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+
+
+feature_list = ['is_Weekend', 'Holiday', '5d rolling avg', '30 rolling avg',
+                'precipitation', 'Average Temp Norm', 'total population',
+                'median income', '% 25 older', '% married', '% highschool graduates',
+                '% foreign', '% poverty', '% own house', 'rides', 'housing_price_1b',
+                'housing_price_2b', 'housing_price_3b']
+label = 'Bin'
 
 
 def create_missing_dirs(path):
@@ -44,12 +54,35 @@ def train_test_split(data):
     return idx_list
 
 
-def train(model, data):
+def convert_pandas_to_tensor(data):
+    """
+    Convert pandas dataframe to torch tensor dataset
+    :param data: pandas dataframe
+    :return: tensor dataset
+    """
+    features = data[feature_list]
+    labels = data[label]
+
+    num_dates = len(set(data['Date']))
+    features = features.reshape((num_dates, 5, 10, len(feature_list)))
+    labels = labels.reshape((num_dates, 5, 10, 1))
+
+    features_tensor = torch.Tensor(features)
+    labels_tensor = torch.Tensor(labels)
+    dataset = TensorDataset(features_tensor, labels_tensor)
+    return dataset
+
+
+def train(model, data, batch_size=5, shuffle=True):
     # TODO: train model
     idx_list = train_test_split(data)
     for idx in idx_list:
         train_data = data[data['quarter_ind'].isin(idx[:2])]
         test_data = data[data['quarter_ind'] == idx[3]]
+        train_dataset = convert_pandas_to_tensor(train_data)
+        test_dataset = convert_pandas_to_tensor(test_data)
+        trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+        testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
     return None
 
 
