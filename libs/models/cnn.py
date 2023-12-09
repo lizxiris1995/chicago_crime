ccb28a187dc58c94a8e649fcd2314dfa9aa55b9f
Zexi Li <zli735@gatech.edu>
2023-12-09 00:41:12 -0500

    update cnn configuration

--- libs/models/cnn.py
+++ libs/models/cnn.py
@@ -15,14 +15,20 @@ class ConvolutionalNetwork(nn.Module):
         self.input_dim = input_dim
         self.num_classes = num_classes
 
-        self.conv1 = nn.Conv2d(in_channels=input_dim, out_channels=6, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=num_classes, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=input_dim, out_channels=20, kernel_size=3, padding=2)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=25, kernel_size=3, padding=2)
+        self.linear1 = nn.Linear(in_features=1000, out_features=50*num_classes)
+        self.activation = nn.ELU()
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = torch.transpose(x, 1, 2)
+        batch_size = x.shape[0]
+        x = self.activation(self.conv1(x))
+        x = self.maxpool1(x)
+        x = self.activation(self.conv2(x))
+        x = torch.reshape(x, (batch_size, -1))
+        x = self.activation(self.linear1(x))
         x = torch.reshape(x, (-1, self.num_classes))
         x = self.softmax(x)
         return x
